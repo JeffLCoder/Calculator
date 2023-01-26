9cf56c4e4e0ac3b81ca83c2228b7fea1e691ac35
JeffLCoder <jeff_lei@outlook.com>
2023-01-26 15:42:58 -0500

    change input type back to text     number type can't parse string ended with . make populating and storing number separate functions removed kbd tag for clear event.target

--- apps.js
+++ apps.js
@@ -1,18 +1,35 @@
 
 // capture  input
-const displayInput = [];
+let displayInput = '';
+const inputNum = [];
 const keys = document.querySelectorAll('.key')
 const display = document.getElementById('display')
 
 for (let key of keys) {
     key.addEventListener('click', (e) => {
-        console.log(e.target.innerText);
         if (!'+-*/='.includes(e.target.innerText)) {
-            displayInput.push(key.innerText);
-            display.value = displayInput.join('');
+            populateNum(key);
+        } else {
+            storeNum();
+            display.value = '';
+            displayInput = '';
         }
+
     }
     )
 }
+function populateNum(elem) {
+    displayInput += elem.innerText;
+    display.value = displayInput;
+}
+function storeNum() {
+    if (!inputNum[0]) {
+        inputNum[0] = parseFloat(displayInput);
+    } else {
+        inputNum[1] = parseFloat(displayInput);
+    }
+}
+
+
 // display.addEventListener('input', e => console.log(e));
 // console.log(displayInput.join(''));
