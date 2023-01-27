1e2fe9315211537bc64e04ab0ce1b37e124ca976
JeffLCoder <jeff_lei@outlook.com>
2023-01-27 16:46:57 -0500

    add doMath for all 4 operations add storeOperator func add logic flow when 1st/2nd num/op input received

--- apps.js
+++ apps.js
@@ -1,35 +1,65 @@
 
 // capture  input
-let displayInput = '';
-const inputNum = [];
-const keys = document.querySelectorAll('.key')
-const display = document.getElementById('display')
+const inputNums = [];
+const inputOperators = [];
+let result = [];
+
+const keys = document.querySelectorAll('.key');
+const display = document.getElementById('display');
+display.value = displayInput = '0';
 
 for (let key of keys) {
     key.addEventListener('click', (e) => {
         if (!'+-*/='.includes(e.target.innerText)) {
             populateNum(key);
+        } else if (inputOperators.length >= 1) {
+            storeOperator(e.target.innerText);
+            storeNum();
+            doMath();
         } else {
             storeNum();
+            storeOperator(e.target.innerText);
             display.value = '';
             displayInput = '';
         }
-
     }
     )
 }
 function populateNum(elem) {
     displayInput += elem.innerText;
     display.value = displayInput;
+    console.log('displayInput', displayInput)
 }
 function storeNum() {
-    if (!inputNum[0]) {
-        inputNum[0] = parseFloat(displayInput);
+    if (!inputNums[0]) {
+        inputNums[0] = parseFloat(displayInput);
     } else {
-        inputNum[1] = parseFloat(displayInput);
+        inputNums[1] = parseFloat(displayInput);
     }
+    console.log('storeNum', inputNums)
+}
+function storeOperator(op) {
+    inputOperators.push(op);
+    console.log('inputOperators', inputOperators)
 }
 
+function doMath() {
+    switch (inputOperators[inputOperators.length - 2]) {
+        case '/':
+            result = !inputNums[1] ? 'ERROR' : inputNums[0] / inputNums[1]
+            break;
+        case '*':
+            result = inputNums[0] * inputNums[1]
+            break;
+        case '+':
+            result = inputNums[0] + inputNums[1]
+            break;
+        case '-':
+            result = inputNums[0] - inputNums[1]
+            break;
+    }
+    display.value = result;
+}
 
 // display.addEventListener('input', e => console.log(e));
 // console.log(displayInput.join(''));
